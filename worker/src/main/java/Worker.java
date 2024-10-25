d27ac64a134cf71b72ad2af2343a834d7e6e1303
geoffrey0pv <geoffreypv00@gmail.com>
2024-10-24 23:44:30 -0500

    feature: Montecarlo logical implemented in the worker

--- worker/src/main/java/Worker.java
+++ worker/src/main/java/Worker.java
@@ -1,41 +1,37 @@
 import com.zeroc.Ice.Communicator;
 import com.zeroc.Ice.ObjectAdapter;
 import com.zeroc.Ice.Util;
-
 import Contract.MasterPrx;
 import Contract.WorkerPrx;
 import implementations.WorkerImp;
 
-
 public class Worker {
     public static void main(String[] args) {
 
-        String workerSubeddNewID = "";
-
         try (Communicator communicator = Util.initialize(args, "properties.cfg")) {
 
-            WorkerImp workerImp = new WorkerImp();
-
+            // con esto creo   el adaptador y obtener el proxy del Master
             ObjectAdapter adapter = communicator.createObjectAdapter("Worker");
-
-            adapter.add(workerImp, Util.stringToIdentity("WorkerEndpoint"));
-
             adapter.activate();
 
-            System.out.println("WORKER INITIATED ON");
-
-            communicator.waitForShutdown();
-
+            // esto va crear el proxy para el Worker
             WorkerPrx worker = WorkerPrx.checkedCast(communicator.stringToProxy("WorkerEndpoint"));
-
             MasterPrx master = MasterPrx.checkedCast(communicator.stringToProxy("MasterEndpoint"));
 
-            workerSubeddNewID = master.subToMaster(worker);
+            if (master == null || worker == null) {
+                throw new Error("Proxies no válidos para Worker o Master");
+            }
 
+            // Aquí me suscribo al Master y obtengo un ID
+            String workerSubscribedID = master.subToMaster(worker);
+            System.out.println("Worker suscrito con ID: " + workerSubscribedID);
 
-            
+            // aquí inicializo mi WorkerImp y le paso el MasterPrx y el workerID
+            WorkerImp workerImp = new WorkerImp(master, workerSubscribedID);
+            adapter.add(workerImp, Util.stringToIdentity("WorkerEndpoint"));
 
-
+            // espero a que el Master me asigne una tarea de cálculo
+            communicator.waitForShutdown();
         }
     }
 }
--- worker/src/main/java/implementations/WorkerImp.java
+++ worker/src/main/java/implementations/WorkerImp.java
@@ -1,30 +1,36 @@
 package implementations;
+
+import java.util.Random;
+
 import com.zeroc.Ice.Current;
 
 import Contract.MasterPrx;
 import Contract.Worker;
 
-
 public class WorkerImp implements Worker {
-
-    private int amountOfPointsToThrow;
-    private int amountOfPointsInsideTheCircle;
-    private int amountOfPointsOutsideTheCircle;
-    private boolean isCalculatingPi;
-    private boolean isMasterConnected;
+    
     private MasterPrx master;
+    private String workerID;
 
-    public WorkerImp(){
-        this.isCalculatingPi = false;
-        this.isMasterConnected = false;
+    public WorkerImp(MasterPrx master, String workerID) {
+        this.master = master;
+        this.workerID = workerID;
     }
 
     @Override
     public void throwPointToCalculatePi(int amountOfPointsToThrow, Current current) {
-        this.amountOfPointsToThrow = amountOfPointsToThrow;
-        this.isCalculatingPi = true;
-        this.amountOfPointsInsideTheCircle = 0;
-        this.amountOfPointsOutsideTheCircle = 0;
-    }
+        int pointsInsideCircle = 0;
+        Random random = new Random();
 
+        for (int i = 0; i < amountOfPointsToThrow; i++) {
+            double x = random.nextDouble() * 2 - 1; 
+            double y = random.nextDouble() * 2 - 1; 
+            if (x * x + y * y <= 1) {  
+                pointsInsideCircle++;
+            }
+        }
+
+        master.reportFromWorkerPiWasCalculated(pointsInsideCircle, workerID);
+        System.out.println("Worker " + workerID + " completó su cálculo con " + pointsInsideCircle + " puntos dentro del círculo.");
+    }
 }
